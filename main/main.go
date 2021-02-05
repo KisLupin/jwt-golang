205775fc2da8b913b4d940aace378e68c66e8f8f
Kis Lupin <linh.nt.iot@gmail.com>
2021-02-05 22:49:16 +0700

    ok

--- main/main.go
+++ main/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/KisLupin/jwt-golang/main/handler"
+	"log"
+	"net/http"
+	"os"
+)
 func main() {
-	//l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handler.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	http.ListenAndServe("9090", sm )
 }
